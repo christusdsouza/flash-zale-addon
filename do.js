24a12c6c7ce93fe671e4b279af7b8e001b65c8e8
christusdsouza <46989701+christusdsouza@users.noreply.github.com>
2020-07-25 02:34:27 +0530

    upload

---
+++ do.js
@@ -0,0 +1,33 @@
+let flag = 1;  //,intervalID;
+browser.browserAction.onClicked.addListener((tab) => {
+    activeTab = tab.id;
+    if (flag == undefined || flag == 1) {
+        browser.notifications.create({
+            "type": "basic",
+            "title": "FlashZale",
+            "message": "clicked; Refresh starts in 10s"
+        });
+        flag = 0;
+        browser.browserAction.setIcon({
+            "path": "icons/stop.svg"
+        });
+        //intervalID = 
+        window.setInterval(() => {
+            browser.tabs.reload()
+                .catch(err => { console.log(err); });
+        },10000);
+        console.log(intervalID);
+    } else {
+        browser.notifications.create({
+            "type": "basic",
+            "title": "FlashZale",
+            "message": "unclicked, Page Refresh stopped"
+        });
+        browser.browserAction.setIcon({
+            "path": "icons/play.svg",
+            "tabId": tab.id
+        });
+        flag = 1;
+        //auto clears the interval LOL
+    }
+})
